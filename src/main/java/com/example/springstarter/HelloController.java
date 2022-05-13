d37bc187784b24df50bf8e13694c2358b595a3b6
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-14 02:55:28 +0600

    refactor code

--- src/main/java/com/example/springstarter/HelloController.java
+++ src/main/java/com/example/springstarter/HelloController.java
@@ -1,13 +1,11 @@
 package com.example.springstarter;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-//    @GetMapping("/")
     @RequestMapping("")
     public String index()
     {
