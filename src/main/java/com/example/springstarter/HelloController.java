98ed40dac8e7102c854546f82ccdcfa7404014bc
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-05-14 02:53:24 +0600

    request mapping

--- src/main/java/com/example/springstarter/HelloController.java
+++ src/main/java/com/example/springstarter/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.springstarter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/api")
+//    @GetMapping("/")
+    @RequestMapping("")
     public String index()
     {
         return "Hello Root";
+    }
+    @RequestMapping("/apiTest")
+    public String apiTest(){
+        return "This is an API";
     }
 }
