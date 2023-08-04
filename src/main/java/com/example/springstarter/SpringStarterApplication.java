5bb1b5072f3dacab437c65e5af8de7fac4c7fd9e
Nasirul Islam <nasirul.siat@outlook.com>
2023-08-05 03:10:07 +0600

    updated pom file and codebase

--- src/main/java/com/example/springstarter/SpringStarterApplication.java
+++ src/main/java/com/example/springstarter/SpringStarterApplication.java
@@ -9,13 +9,12 @@ public class SpringStarterApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(SpringStarterApplication.class, args);
 		SpringApplication.run(RestApiDemoApplication.class, args);
-    RestTemplate restTemplate = new RestTemplate();
-    String apiUrl = "https://jsonplaceholder.typicode.com/posts";
-    Post[] posts = restTemplate.getForObject(apiUrl, Post[].class);
+		RestTemplate restTemplate = new RestTemplate();
+		String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+		Post[] posts = restTemplate.getForObject(apiUrl, Post[].class);
 
-    for (Post post : posts) {
-      System.out.println(post);
-   }
+		for (Post post : posts) {
+		  System.out.println(post);
+	   }
 	}
-
 }
